c95fcd635d3ec21051318d2a1c9392d8df49f521
Jean Pires <jean.m.pires@gmail.com>
2023-06-19 17:02:58 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/jeanpiress/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanpiress.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
