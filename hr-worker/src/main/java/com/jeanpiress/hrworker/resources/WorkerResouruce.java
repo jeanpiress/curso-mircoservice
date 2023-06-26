104fcda3971cb452ace4d3115dda01f3641c6146
Jean Pires <jean.m.pires@gmail.com>
2023-06-26 09:15:02 -0300

    Zull timeout

--- hr-worker/src/main/java/com/jeanpiress/hrworker/resources/WorkerResouruce.java
+++ hr-worker/src/main/java/com/jeanpiress/hrworker/resources/WorkerResouruce.java
@@ -39,14 +39,14 @@ public class WorkerResouruce {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity <Worker> findById(@PathVariable Long id){
 		
-		/*
+		
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker worker = repository.findById(id).get();
